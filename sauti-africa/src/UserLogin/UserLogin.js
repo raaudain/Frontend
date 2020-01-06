93b4b02e459613455bd992a2d07897b6d4551aeb
Derek <derekdyer02@gmail.com>
2020-01-06 13:33:09 -0800

    initial component commit

---
+++ sauti-africa/src/UserLogin/UserLogin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { 
+    Button, Container, Row, Col, Nav, NavItem, NavLink
+} from 'reactstrap';
+import axios from 'axios';
+
+
+const login = () => {
+
+}
+
+export default login;
