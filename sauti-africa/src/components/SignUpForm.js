1fd0bd03a0304f49321e0edebf263ed8d639a606
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-01-07 08:49:43 -0800

    Completed styling for Sign up page, added fonts and font awesome to index.html

--- sauti-africa/src/components/SignUpForm.js
+++ sauti-africa/src/components/SignUpForm.js
@@ -2,7 +2,11 @@ import React , { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from 'formik';
 import axios from 'axios';
 // import { Button, Label, Input, FormGroup } from 'reactstrap';
+import NextBackNavigation from './NextBackNavigation';
+import SignUpFormImage from '../assets/sign-up-picture.png'
 
+
+const img = `url('${SignUpFormImage}')`;
 
 
 const SignUpForm = () => {
@@ -21,56 +25,91 @@ useEffect(() => {
 },[]);
 
 const inputStyle = {
-    width: '500px',
+
     marginBottom: '20px',
     fontFamily: 'Quicksand',
     border: '1px solid #000000',
     borderRadius: '4px',
     paddingLeft: '10px',
-    fontSize: '1rem'
+    fontSize: '1rem',
+    width: '100%'
 }
 
 const labelStyle = {
     fontFamily: 'Raleway',
-    color: '#000000'
+    color: '#000000',
+    textAlign: 'left',
+    padding: '20px',
+    fontWeight: '600',
+    fontSize: '12px',
+    lineHeight: '18px'
 }
 
 const containerStyle = {
     display: 'flex',
     flexFlow: 'row wrap',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    width: '100%'
 }
 
 const formStyle = {
     display: 'flex',
     flexDirection: 'column',
+    alignItems: 'left',
+    justifyContent: 'center',
+    // minHeight: '50vh',
+    width: '100%',
+    margin: '20px'
+}
+
+const imgContainer = {
+    backgroundImage: img,
+    height: '135px',
+    display: 'flex',
+    justifyContent: 'center',
     alignItems: 'center',
-    alignItems: 'center',
-  justifyContent: 'center',
-  minHeight: '100vh',
-  
+    color: 'white',
+    fontFamily: 'Montserrat',
+fontStyle: 'normal',
+fontWeight: 'normal',
+fontSize: '48px',
+lineHeight: '59px'
+}
+
+const buttonStyle = {
+    background: '#000000',
+    borderRadius: '4px',
+    color: 'white',
+    fontFamily: 'Quicksand',
+    fontStyle: 'normal',
+    fontWeight: 'bold'
+
 }
 
 
+
     return (
-        <div style={containerStyle} className='sign-up-container'>
 
+        <div >
+            <NextBackNavigation />
+            <div style={imgContainer} className='imageContainer'>
+                <h2>Sauti</h2>
+            </div>
+            <div style={containerStyle} className='sign-up-container'>
 
+                <Form style={formStyle}>
 
+                        <h2 style={labelStyle}>YOUR INFORMATION</h2>
+                        <Field style={inputStyle} type="text" name="username"  placeholder="USERNAME" />
+                        <Field style={inputStyle} type="email" name="email"  placeholder="EMAIL" />
+                        <Field style={inputStyle} type="password" name="password" placeholder="PASSWORD" />
+                
+                    <button style={buttonStyle} type='submit'>Sign Up</button>
 
-            <Form style={formStyle}>
+                </Form>
+            </div>
 
-
-                    <h2 style={labelStyle}>YOUR INFORMATION</h2>
-                    <Field style={inputStyle} type="text" name="username"  placeholder="USERNAME" />
-                    <Field style={inputStyle} type="email" name="email"  placeholder="EMAIL" />
-                    <Field style={inputStyle} type="password" name="password" placeholder="PASSWORD" />
-            
-                <button type='submit'>Submit</button>
-
-            </Form>
         </div>
-
 
     )
 }
