89fefb3c2aba94aa54b1be081c75879e46967519
MarianaShebanova <mariana.shebanova@gmail.com>
2020-01-09 08:46:53 -0800

    ADD DEL EDIT

--- sauti-africa/src/utils/axiosWithAuth.js
+++ sauti-africa/src/utils/axiosWithAuth.js
@@ -3,9 +3,8 @@ import axios from 'axios';
 export const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
     return axios.create({
-        baseURL: 'http://localhost:5000/api',
         headers: {
-            Authorization: token
+            'Authorization': `${token}`
         }
     });
 };
